b15d335b0eb620db6ef9e0ebe4d06c008ad6bd26
ComputerFish <52689119+ComputerFish@users.noreply.github.com>
2025-08-17 19:08:07 -0700

    enabled basic message passing functionality

--- AirSerial-Client/src/main.cpp
+++ AirSerial-Client/src/main.cpp
@@ -1,18 +1,47 @@
-#include <Arduino.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define CE_PIN 17
+#define CSN_PIN 5
 
+RF24 radio(CE_PIN, CSN_PIN);
+const byte address[6] = "00001";
+
+#define CLIENT_BAUDRATE 115200
+
+// Setup radio settings
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(CLIENT_BAUDRATE);
+
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setRetries(5, 15);
+  radio.setDataRate(RF24_250KBPS);
+  radio.setPALevel(RF24_PA_LOW);
+  radio.setCRCLength(RF24_CRC_16);
+  radio.startListening();
 }
 
+// Basic Flow:
+// 1. Check if data is available from the RF24 radio
+// 2. If available, read the data into a buffer
+// 3. Forward the data to the client Serial
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  if (radio.available()) {
+    char buffer[32];
+    uint8_t len = radio.getDynamicPayloadSize(); // get actual size
+    if (len == 0 || len > 32) len = 32;          // safety clamp
+    radio.read(buffer, len);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    // Forward to client
+    for (uint8_t i = 0; i < len; i++) {
+      Serial.write(buffer[i]);
+    }
+
+    // Debug
+    // Serial.print("Forwarded ");
+    // Serial.print(len);
+    // Serial.println(" bytes to client");
+  }
 }
--- AirSerial-Server/src/main.cpp
+++ AirSerial-Server/src/main.cpp
@@ -1,18 +1,50 @@
-#include <Arduino.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define CE_PIN 17
+#define CSN_PIN 5
 
+RF24 radio(CE_PIN, CSN_PIN);
+const byte address[6] = "00001";
+
+// Setup radio settings
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  radio.begin();
+  radio.setRetries(5, 15);         // delay=5*250µs=1250µs, count=15
+  radio.setPALevel(RF24_PA_LOW);   // Power amplifier level
+  radio.setDataRate(RF24_250KBPS); // more robust link
+  radio.setCRCLength(RF24_CRC_16);
+  radio.openWritingPipe(address);
+  radio.stopListening();
 }
 
+// Basic flow:
+// Read data from physical serial port
+// and forward it to RF24 radio to transmit
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  static char buffer[32];
+  static uint8_t index = 0;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  while (Serial.available()) {
+    char c = Serial.read();
+    buffer[index++] = c;
+
+    // If buffer full, send immediately
+    if (index >= sizeof(buffer)) {
+      radio.write(buffer, index);
+      index = 0;
+    }
+  }
+
+  // Flush partial buffer if some data is waiting and no new bytes arrive
+  if (index > 0) {
+    delay(2); // small gap to see if more bytes arrive
+    if (!Serial.available()) {
+      radio.write(buffer, index);
+      index = 0;
+    }
+  }
 }
